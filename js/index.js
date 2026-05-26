372ee23443dd873c292534dadcd2ead1b34925f4
Juanpidk <150383660+angieherrerauptc@users.noreply.github.com>
2026-05-25 23:27:28 -0500

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,5 @@
+// Archivo JavaScript del inicio.
+// Por ahora no necesita funciones obligatorias.
+// Lo dejamos separado para que el proyecto quede más organizado.
+
+console.log("Index cargado correctamente");
